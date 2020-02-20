6e65fe31b74424be96af40ff7d99aabc4acef7b2
Coban Tudor <cobantudor@gmail.com>
2020-02-20 08:20:57 +0200

    Adjust configuration class

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
 class Config:
+    LOGS_FILE = 'logs/workers.txt'
     DATA_FILES_PATH = '/data/'
     MONGODB_HOST = 'mongo'
-    MONGODB_PORT = '27017'
+    MONGODB_PORT = 27017
     MONGODB_DB = 'admin'
     POSTGRES_HOST = 'postgress'
-    POSTGRES_PORT = '5432'
+    POSTGRES_PORT = 5432
     POSTGRES_USER = 'root'
     POSTGRES_PASSWORD = 'root'
     POSTGRES_DATABASE = 'postgress'
